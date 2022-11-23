fbbab59b392dc38d55417811bc26617d47ce397f
user <user>
2022-11-23 11:10:35 +0100

    Ricostruzione struttura API 5E

--- API/Product_done/checkProduct.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("../../DB/connect.php");
-require("../../MODEL/productController.php");
-
-
-$controller = new ProductController($conn);
-
-$controller->CheckProduct();
-?>
--- API/readme.txt
+++
@@ -1,1 +0,0 @@
-Modonesi: i le cartelle _done sono ritenute terminate per quanto riguarda la creazione di api

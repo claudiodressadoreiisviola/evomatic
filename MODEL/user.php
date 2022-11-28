63a67d0587776e5bacf7e5da254debcc1e0cfbbc
Nico <nicolociancaglia@gmail.com>
2022-11-28 19:56:22 +0100

    User model con quasi tutte le query

--- MODEL/user.php
+++ MODEL/user.php
@@ -1,13 +1,114 @@
 <?php
+
+spl_autoload_register(function ($class) {
+    require __DIR__ . "/../COMMON/$class.php";
+});
+
+set_exception_handler("errorHandler::handleException");
+set_error_handler("errorHandler::handleError");
+
 class User
 {
-    private $table_name;
+    private Connect $db;
+    private PDO $conn;
 
-    private $id;
-    private $name;
-    private $surname;
-    private $email;
-    private $password;
-    private $active;
+    public function __construct()
+    {
+        $this->db = new Connect;
+        $this->conn = $this->db->getConnection();
+    }
+
+    public function getUser($id)
+    {
+        $sql = "SELECT name, email
+            FROM account
+            WHERE id = :id";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function deleteUser($id)
+    {
+        $sql = "UPDATE account 
+            SET active = 0 
+            WHERE id = :id";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+
+        return $stmt->execute();
+    }
+
+    public function resetPassword($id)
+    {
+        $date = date("d:m:Y h:i:s");
+        $password = "temporanea";
+
+        // Update password con password temporanea
+        $sql = "UPDATE account
+            SET password = :password
+            WHERE id = :id";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+
+        $firstQuery = $stmt->execute();
+
+        // Aggiunta alla tabella reset l'utente
+        $sql = "INSERT INTO reset
+            (user, password, requested, expires, completed)
+            VALUES (:user, :password, :requested, :expires, :completed)";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':user', $id, PDO::PARAM_INT);
+        $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+        $stmt->bindValue(':requested', $date, PDO::PARAM_STR);
+        $stmt->bindValue(':completed', date("d:m:Y h:i:s", strtotime($date . '+ 5 Days')), PDO::PARAM_STR);
+        $stmt->bindValue(':completed', 0, PDO::PARAM_INT);
+
+        $secondQuery = $stmt->execute();
+
+        return $firstQuery && $secondQuery;
+    }
+
+    public function login($id, $email, $password)
+    {
+        $sql = "SELECT count(:id)
+        FROM account 
+        WHERE email = :email AND password = :password";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function changePassword($id, $email, $password, $newPassword)
+    {
+        if ($this->login($id, $email, $password) == 0) {
+            $sql = "UPDATE account 
+            SET password = :newPassword
+            WHERE email = :email AND password = :password";
+
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+            $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+            $stmt->bindValue(':newPassword', $newPassword, PDO::PARAM_STR);
+            $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+
+            $stmt->execute();
+
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+    }
 }
-?>

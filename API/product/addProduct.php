c3cb083e898e3c312714e4b01b582f5802552c76
claudiodressadoreiisviola <118843465+claudiodressadoreiisviola@users.noreply.github.com>
2023-01-10 17:57:39 +0100

    Prima stesura addProduct

---
+++ API/product/addProduct.php
@@ -0,0 +1,20 @@
+<?php
+require __DIR__ . '/../../MODEL/cart.php';
+header("Content-type: application/json; charset=UTF-8");
+
+$data = json_decode(file_get_contents("php://input"));
+
+if(empty($data->user) || empty($data->product) || empty($data->quantity)){
+    http_response_code(400);
+    echo json_encode(["message" => "Fill every field"]);
+}
+
+$cart = new Cart;
+
+if($cart -> addCartProduct($data->user, $data->product, $data->quantity)){
+    http_response_code(200);
+    echo json_encode(["message" => "Product added successfully"]);
+}else{
+    http_response_code(400);
+    echo json_encode(["message" => "Fill every field"]);
+}

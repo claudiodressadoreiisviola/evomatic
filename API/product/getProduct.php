1b08aada150bf6e91a7155196e083ada9d3d275a
Riccardo-Barchi <90723294+Riccardo-Barchi@users.noreply.github.com>
2023-01-17 18:55:56 +0100

    Update getProduct.php

--- API/product/getProduct.php
+++ API/product/getProduct.php
@@ -1,5 +1,6 @@
 <?php
 require __DIR__ . '/../../MODEL/product.php';
+require __DIR__ . '/../../MODEL/favourite.php';
 header("Content-type: application/json; charset=UTF-8");
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
@@ -11,11 +12,29 @@ if (empty($parts[5])) {
 }
 
 $product = new Product();
+$favourite = new Favourite();
+$fav = false;
 
 $result = $product->getProduct($parts[5]);
+$resultFav = $favourite->getArchiveFavourite($parts[6]);
+for ($j = 0; $j < (count($resultFav)); $j++){
+    if ($resultFav[$j]["id"] == $parts[5])
+        $fav = true;
+}
+$nutritionalValue = $product->getNutritionalValue($parts[5]);
+$productArchiveProduct = array(
+    "id" => $parts[5],
+    "name" => $result["name"],
+    //"category" => $result["category"], 
+    "price" => $result["price"],
+    "description" => $result["description"],
+    "quantity" => $result["quantity"],
+    "nutritional value" => $nutritionalValue,
+    "favourite" => $fav
+);
 
 if ($result != false) {
-    echo json_encode($result);
+    echo json_encode($productArchiveProduct);
 } else {
     http_response_code(400);
     echo json_encode(["message" => "Product not found"]);

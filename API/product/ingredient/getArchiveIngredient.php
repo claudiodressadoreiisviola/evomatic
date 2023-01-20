65b7f0361df96ec31e7cb212e2b2fe0cf93e9780
Riccardo-Barchi <90723294+Riccardo-Barchi@users.noreply.github.com>
2023-01-20 16:20:00 +0100

    Update getArchiveIngredient.php

--- API/product/ingredient/getArchiveIngredient.php
+++ API/product/ingredient/getArchiveIngredient.php
@@ -1,5 +1,4 @@
 <?php
-/*
 require __DIR__ . '/../../../MODEL/ingredient.php';
 header("Content-type: application/json; charset=UTF-8");
 
@@ -8,5 +7,5 @@ $ingredient = new Ingredient;
 $result = $ingredient->getArchiveIngredient();
 
 echo json_encode($result);
-*/
+
 ?>

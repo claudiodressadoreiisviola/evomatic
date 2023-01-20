b337e66c12944a0d1e9055e74ca1b1fd4a935a90
UnTizioCheEsiste <40698981+UnTizioCheEsiste@users.noreply.github.com>
2023-01-20 16:50:15 +0100

    Create getNutritionalValue.php

---
+++ API/product/getNutritionalValue.php
@@ -0,0 +1,35 @@
+<?php
+require __DIR__ . '/../../MODEL/product.php';
+header("Content-type: application/json; charset=UTF-8");
+
+$parts = explode("/", $_SERVER["REQUEST_URI"]);
+
+if (empty($parts[5])) {
+    http_response_code(404);
+    echo json_encode(["message" => "Insert a valid ID"]);
+    exit();
+}
+
+$product = new Product();
+
+$result = $product->getNutritionalValue($parts[5]);
+
+$nutritionalValue = array(
+  "kcal" =>  $result[$i]["kcal"],
+  "fats" => $result[$i]["fats"],
+  "saturated_fats" => $result[$i]["saturated_fats"],
+  "carbohydrates" => $result[$i]["carbohydrates"],
+  "sugars" => $result[$i]["sugars"],
+  "proteins" => $result[$i]["proteins"],
+  "fiber" => $result[$i]["fiber"],
+  "salt" => $result[$i]["salt"],
+);
+
+
+
+if (empty($nutritinalValue)) {
+    http_response_code(404);
+} else {
+    http_response_code(200);
+    echo json_encode($nutritinalValue);
+}

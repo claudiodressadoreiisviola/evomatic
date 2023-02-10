82540414400e6a70b66c8f68ba67e6e257e77ac1
claudiodressadoreiisviola <118843465+claudiodressadoreiisviola@users.noreply.github.com>
2023-02-10 19:16:18 +0100

    Correggo modello

--- API/user/registerAccount.php
+++ API/user/registerAccount.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '/../../MODEL/product.php';
+require __DIR__ . '/../../MODEL/user.php';
 header("Content-type: application/json; charset=UTF-8");
 
 $data = json_decode(file_get_contents("php://input"));
@@ -10,9 +10,9 @@ if (empty($data->name) || empty($data->surname) || empty($data->email) || empty(
     die();
 }
 
-$product = new Product();
+$user = new User();
 
-$result = $product->registerUser($name, $surname, $email, $password, $year, $section, $schoolYear, $active);
+$result = $user->registerUser($name, $surname, $email, $password, $year, $section, $schoolYear, $active);
 
 echo json_encode($result);
 ?>

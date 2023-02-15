cc35a846cfbd29170b7f282054a6feca122e705e
Riccardo-Barchi <90723294+Riccardo-Barchi@users.noreply.github.com>
2023-02-15 22:16:25 +0100

    Update getIngredient.php

--- API/product/ingredient/getIngredient.php
+++ API/product/ingredient/getIngredient.php
@@ -1,13 +1,12 @@
 <?php
-/*
 require __DIR__ . '/../../../MODEL/ingredient.php';
-
+header("Content-type: application/json; charset=UTF-8");
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
 $ingredient = new Ingredient;
 
-$result = $ingredient->getIngredient($parts[5]);
+$result = $ingredient->getIngredient($parts[6]);
 
 echo json_encode($result);
-*/
+
 ?>

425a8775f1118eca35b288b485d23f347b76dd35
UnTizioCheEsiste <40698981+UnTizioCheEsiste@users.noreply.github.com>
2023-02-20 19:07:12 +0100

    Update login.php

--- API/user/login.php
+++ API/user/login.php
@@ -3,7 +3,7 @@ require __DIR__ . '/../../MODEL/user.php';
 header("Content-type: application/json; charset=UTF-8");
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin');
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     header('HTTP/1.1 200 OK');

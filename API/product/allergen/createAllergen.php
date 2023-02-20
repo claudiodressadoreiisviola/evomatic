97f29d0af0ed2b368483f916099e26ea7242a210
Riccardo-Barchi <90723294+Riccardo-Barchi@users.noreply.github.com>
2023-02-20 17:38:18 +0100

    Update createAllergen.php

--- API/product/allergen/createAllergen.php
+++ API/product/allergen/createAllergen.php
@@ -2,12 +2,19 @@
 // ERRATO -> API solo paninara
 
 require __DIR__ . '/../../../MODEL/allergen.php';
+header("Content-type: application/json; charset=UTF-8");
 
-$parts = explode("/", $_SERVER["REQUEST_URI"]);
+$data = json_decode(file_get_contents("php://input"));
+
+if (empty($data->name)) {
+    http_response_code(400);
+    echo json_encode(["message" => "Fill every field"]);
+    die();
+}
 
 $allergen = new Allergen;
 
-$result = $allergen->createAllergen($name);
+$result = $allergen->createAllergen($data->name);
 
 echo json_encode($result);
 ?>
